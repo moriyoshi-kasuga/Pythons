53ddf208816f119b6bb76c68ac29bbb9ce406940
moriyoshi-kasug <moriyoshi.kasuga1218@gmail.com>
2023-07-15 23:58:00 +0900

    delete file

--- small/line_list.py
+++
@@ -1,9 +0,0 @@
-print("数字を空白で分けて入力してください(一行ずつlistにしてそれらを入れたlistを出力します)")
-
-list_all: list[list[int]] = []
-while True:
-    string = input("入力してください(cで終了します): ")
-    if string == "c":
-        break
-    list_all.append(list(map(int, string.split())))
-print(list_all)
