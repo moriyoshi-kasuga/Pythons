56b43b94afe900d4f8d16ed2e1f3f95b2565b706
moriyoshi-kasug <moriyoshi.kasuga1218@gmail.com>
2023-07-17 21:01:51 +0900

    base django todo application

--- RYAMA/RYAMA/base/urls.py
+++ RYAMA/RYAMA/base/urls.py
@@ -1,5 +1,17 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 
-from . import views
+from .views import (CustomLoginView, RegisterPage, TaskCreate, TaskDelete,
+                    TaskDetail, TaskList, TaskUpdate, task_complete)
 
-urlpatterns = [path("", views.taskList, name="tasks")]
+urlpatterns = [
+    path("register/", RegisterPage.as_view(), name="register"),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
+    path("", TaskList.as_view(), name="tasks"),
+    path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
+    path("task-create/", TaskCreate.as_view(), name="task-create"),
+    path("task-update/<int:pk>/", TaskUpdate.as_view(), name="task-update"),
+    path("task-complete/<int:pk>/", task_complete, name="task-complete"),
+    path("task-delete/<int:pk>/", TaskDelete.as_view(), name="task-delete"),
+]
