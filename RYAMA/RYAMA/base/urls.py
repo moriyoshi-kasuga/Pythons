39adc6fcea2f76b1b4d34fe3a6817707055a9e22
moriyoshi-kasug <moriyoshi.kasuga1218@gmail.com>
2023-07-19 14:47:02 +0900

    urls

--- RYAMA/RYAMA/base/urls.py
+++ RYAMA/RYAMA/base/urls.py
@@ -1,17 +1,33 @@
 from django.contrib.auth.views import LogoutView
+from django.shortcuts import render
 from django.urls import path
 
-from .views import (CustomLoginView, RegisterPage, TaskCreate, TaskDelete,
-                    TaskDetail, TaskList, TaskUpdate, task_complete)
+from .views import (
+    TaskCreate,
+    TaskDelete,
+    TaskDetail,
+    TaskList,
+    TaskUpdate,
+    page_about,
+    page_account,
+    page_home,
+    page_service,
+)
 
 urlpatterns = [
-    path("register/", RegisterPage.as_view(), name="register"),
-    path("login/", CustomLoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
-    path("", TaskList.as_view(), name="tasks"),
+    path(
+        "test/",
+        lambda request, *args, **kwargs: render(request, "base/test.html"),
+        name="test",
+    ),
+    path("about/", page_about, name="about"),
+    path("service/", page_service, name="service"),
+    path("account/", page_account, name="account"),
+    path("logout/", LogoutView.as_view(next_page="home"), name="logout"),
+    path("", page_home, name="home"),
     path("task/<int:pk>/", TaskDetail.as_view(), name="task"),
+    path("tasks/", TaskList.as_view(), name="tasks"),
     path("task-create/", TaskCreate.as_view(), name="task-create"),
     path("task-update/<int:pk>/", TaskUpdate.as_view(), name="task-update"),
-    path("task-complete/<int:pk>/", task_complete, name="task-complete"),
     path("task-delete/<int:pk>/", TaskDelete.as_view(), name="task-delete"),
 ]
