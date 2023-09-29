65e46717158e68a53accfc622d2f5c419b95e8ad
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2023-09-29 11:00:12 +0900

    Fix one line

--- scraping/five.py
+++ scraping/five.py
@@ -1,27 +1,13 @@
 from bs4 import BeautifulSoup
 from requests_html import HTMLSession
 
-# r = HTMLSession().get("https://www.mizuhobank.co.jp/rate_fee/rate_interest.html")
-# r.html.render()
-#
-#
-# [
-#     print(c.find("td").text + " : " + c.find("th").text)
-#     for c in BeautifulSoup(r.html.raw_html, "html.parser")
-#     .select_one("table[data-market-id='bk07'] tbody")
-#     .findChildren("tr")
-# ]
+r = HTMLSession().get("https://www.mizuhobank.co.jp/rate_fee/rate_interest.html")
+r.html.render()
 
 
 [
-    r.find("td").text + " : " + r.find("th").text
-    for r in BeautifulSoup(
-        HTMLSession()
-        .get("https://www.mizuhobank.co.jp/rate_fee/rate_interest.html")
-        .html.render()
-        .raw_html,
-        "html.parser",
-    )
+    print(c.find("td").text + " : " + c.find("th").text)
+    for c in BeautifulSoup(r.html.raw_html, "html.parser")
     .select_one("table[data-market-id='bk07'] tbody")
     .findChildren("tr")
 ]
